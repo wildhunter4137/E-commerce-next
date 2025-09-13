d75654c16ad18d99dc8cc81896c2af0a57c638c2
immortal77413 <immortal4137@gmail.com>
2025-09-13 18:48:05 -0400

    fix

--- pages/unauthorized.js
+++ pages/unauthorized.js
@@ -1,19 +1,17 @@
-import Layout from "@/components/Layout";
-import { useRouter } from "next/router";
-import React from "react";
+import Layout from '@/components/Layout';
+import { useRouter } from 'next/router';
+import React from 'react';
 
 function Unauthorized() {
-  const router = useRouter();
-  const { message } = router.query;
+	const router = useRouter();
+	const { message } = router.query;
 
-  return (
-    <Layout title="Unauthorized Page">
-      <h1 className="text-xl">Access Denied</h1>
-      <div className="flex">
-        {message && <div className="mb-4 text-red-600">{message}</div>}
-      </div>
-    </Layout>
-  );
+	return (
+		<Layout title='Unauthorized Page'>
+			<h1 className='text-xl'>Access Denied. Please contact website owner.</h1>
+			<div className='flex'>{message && <div className='mb-4 text-red-600'>{message}</div>}</div>
+		</Layout>
+	);
 }
 
 export default Unauthorized;
